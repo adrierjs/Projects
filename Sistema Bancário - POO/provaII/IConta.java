0a5768ae3bd85ad3ef1fd6c2b16df589529b247f
Adrier <adrierj@gmail.com>
2022-06-27 00:43:51 -0300

    Sistema_bancario_POO

---
+++ Sistema Bancário - POO/provaII/IConta.java
@@ -0,0 +1,17 @@
+package provaII;
+
+/**
+ *
+ * @author adrie
+ */
+public interface IConta {
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, Conta contaDestino);
+
+    void imprimir();
+
+}
